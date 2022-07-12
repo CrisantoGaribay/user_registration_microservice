2c18141c22db2005965be056d04ec6a2735a821e
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-12 16:38:19 -0700

    Fixes update document

--- src/main/java/com/unosquare/user_registration/model/User.java
+++ src/main/java/com/unosquare/user_registration/model/User.java
@@ -2,6 +2,7 @@ package com.unosquare.user_registration.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
@@ -10,6 +11,9 @@ import java.time.LocalDate;
 @Builder
 @Document(collection = "USERS")
 public class User {
+
+    @Id
+    private String id;
 
     private String first_name;
 
